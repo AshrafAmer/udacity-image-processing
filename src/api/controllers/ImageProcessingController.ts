47054619a27322c9589d2468c093c6e386fb4121
Ashraf Amer <ashraf.amer55@gmail.com>
2022-02-19 21:57:46 +0200

    try catch endpoint

--- src/api/controllers/ImageProcessingController.ts
+++ src/api/controllers/ImageProcessingController.ts
@@ -6,37 +6,31 @@ const imageProcessingRouter = express.Router();
 imageProcessingRouter.get(
     '/',
     async (req: express.Request, res: express.Response) => {
-        // validate url queries
-        const params = ImageProcessingValidate.mapQuery(req);
         try {
+            // validate url queries
+            const params = ImageProcessingValidate.mapQuery(req);
             ImageProcessingValidate.validate(params);
-        } catch (err: unknown) {
-            res.status(400).send(`${err}`);
-        }
 
-        // validate image existance
-        try {
+            // validate image existance
             await ImageProcessingValidate.validImageName(params.filename);
-        } catch (err: unknown) {
-            console.log(`error: ${err}`);
-            res.status(400).send(`error occured: ${err}`);
-        }
+            const outputImageName = `${params.filename}_${params.width}_${params.height}`;
+            // check folder thumb existance
+            await ImageProcessingService.checkOutputThumbExistance();
+            const outputImage = await ImageProcessingService.outputImagePath(
+                outputImageName
+            );
+            const image = await ImageProcessingService.imagePath(
+                params.filename
+            );
 
-        const outputImageName = `${params.filename}_${params.width}_${params.height}`;
-        // check folder thumb existance
-        await ImageProcessingService.checkOutputThumbExistance();
-        const outputImage = await ImageProcessingService.outputImagePath(
-            outputImageName
-        );
-        const image = await ImageProcessingService.imagePath(params.filename);
+            // validate not created before (Cashed images)
+            const isExist = await ImageProcessingValidate.isExist(
+                outputImageName
+            );
+            if (isExist) {
+                return res.status(200).sendFile(outputImage);
+            }
 
-        // validate not created before (cached images)
-        const isExist = await ImageProcessingValidate.isExist(outputImageName);
-        if (isExist) {
-            return res.status(200).sendFile(outputImage);
-        }
-
-        try {
             // resize image
             await ImageProcessingService.imageSharp({
                 imageName: image,
@@ -44,12 +38,11 @@ imageProcessingRouter.get(
                 height: Number(params.height),
                 outputFile: outputImage,
             });
-        } catch (err: unknown) {
-            console.log(`error: ${err}`);
-            res.status(400).send(`error occured: ${err}`);
-        }
 
-        res.status(200).sendFile(outputImage);
+            res.status(200).sendFile(outputImage);
+        } catch (err: unknown) {
+            res.status(400).send(`${err}`);
+        }
     }
 );
 
